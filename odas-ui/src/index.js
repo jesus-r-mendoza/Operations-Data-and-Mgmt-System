6c0dda9d5ff488e87d90a04f0a8c7b1d66547033
Maximilian Orozco <maxorozco213@gmail.com>
2019-10-01 17:11:24 -0700

    Implemented header

--- odas-ui/src/index.js
+++ odas-ui/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Layout/Header';
+import {Navbar, NavbarBrand, NavDropdown, Nav, NavItem, Container} from "react-bootstrap";
+import DropdownMenu from "react-bootstrap/DropdownMenu";
+import NavLink from "react-bootstrap/NavLink";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Styles from './Layout/Styles.css'
 
 class App extends React.Component {
     constructor(props) {
@@ -10,10 +14,31 @@ class App extends React.Component {
     render() {
         return (
             //page container
-            <div>
-                {/*header container*/}
-                    <Header>Operations Data and Management System</Header>
-            </div>
+            <Navbar fixed={'top'} variant={'light'}>
+                <NavbarBrand href={"#index"}>Operations Data and Management System</NavbarBrand>
+                <Container className={'justify-content-end'}>
+                    <NavDropdown title={"Generate a Report"}>
+                        <NavDropdown.Item>
+                            <NavLink>Dataset</NavLink>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item>
+                            <NavLink>
+                                Query a Dataset
+                            </NavLink>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item>
+                            <NavLink>
+                                Generate a Dataset w/ COSMOS
+                            </NavLink>
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavItem className={'justify-content-end'}>
+                        <NavLink>
+                            About
+                        </NavLink>
+                    </NavItem>
+                </Container>
+            </Navbar>
         )
     }
 }
