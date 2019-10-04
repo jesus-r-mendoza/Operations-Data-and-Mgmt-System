68aef3ea4f7a86195caf97249ba8527efb56479d
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-10-04 09:37:55 -0700

    created simple satellite model

--- backend/odas_backend/odas/models.py
+++ backend/odas_backend/odas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Satellite(model.Models):
+    name = models.CharField(max_length=64)
+    mission_description = models.CharField(max_length=512)
+    year_lauched = models.DateTimeField(auto_now_add=True)
