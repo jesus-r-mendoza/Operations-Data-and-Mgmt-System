998d19c27d6ee6999fd586b2bc07bc651df380cb
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-10-04 09:50:01 -0700

    serving satellite data as api at 'localhost:8000/api/satellites/' -> when server running

--- backend/odas_backend/odas/urls.py
+++ backend/odas_backend/odas/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .api import SatelliteViewSet
 
 router = routers.DefaultRouter()
-router.register('api/satellites/', SatelliteViewSet, 'satellites/')
+router.register('api/satellites', SatelliteViewSet, 'satellites')
 
 urlpatterns = router.urls
