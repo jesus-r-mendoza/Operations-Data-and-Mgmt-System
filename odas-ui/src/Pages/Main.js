787497fe808fc21a4ce1982948dc3c57ccd191b2
Maximilian Orozco <maxorozco213@gmail.com>
2019-10-04 14:55:02 -0700

    Added email form; rearranged UI items

--- odas-ui/src/Pages/Main.js
+++ odas-ui/src/Pages/Main.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+// Stylesheets
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Badge, Button, FormControl} from "react-bootstrap";
+import "../Layout/MainStyles.css"
 
 export default class Main extends React.Component {
-
     render() {
-        return(
+        return (
             <div className={"main-page"}>
-                <div>
-                    <h1>MAIN PAGE</h1>
-                    <img src={require("../Images/front-page.gif")} />
+                <div className={"main-container"}>
+                    <div id={"form"}>
+                        <span className={"main-text"}>Welcome to ODAS Report Generator</span>
+                        <div className={"main-button"}>
+                            <Link to={"/query"}>
+                                <Button size={"lg"} variant={"info"}>Query a Dataset</Button>
+                            </Link>
+                            <Link to={"/generate"}>
+                                <Button size={"lg"} variant={"info"}>Generate a Dataset</Button>
+                            </Link>
+                            <Link to={"/upload"}>
+                                <Button size={"lg"} variant={"info"}>Upload a Dataset</Button>
+                            </Link>
+                        </div>
+                        <div className={"email-input"}>
+                            <div className={"email-header"}>
+                                <span className={"email-header"}>Input an email to receive reports</span>
+                                <Button size={"sm"} variant={"outline-info"} type={"submit"}>Info</Button>
+                            </div>
+                            <FormControl type={"text"} placeholder={"Email"} />
+                            <div className={"email-button"}>
+                                <Button size={"sm"} variant={"info"} type={"submit"}>Sign Up</Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div id={"img"} className={"main-image"}>
+                        <img src={require("../Images/front-page.gif")} alt={""} />
+                    </div>
                 </div>
             </div>
         );
--- odas-ui/src/index.js
+++ odas-ui/src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route} from "react-router-dom";
-
+// Components
 import Header from "./Layout/Header";
 import Main from "./Pages/Main";
 import QueryData from "./Pages/QueryData";
 import GenerateData from "./Pages/GenerateData";
 import UploadData from "./Pages/UploadData";
+// Stylesheets
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // TODO consider putting the router in its own file
 // TODO Consider turning into a nested route
