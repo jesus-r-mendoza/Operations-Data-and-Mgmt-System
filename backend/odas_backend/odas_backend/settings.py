01831c48a332d8cec856c0b0a71004fe88cbf5f5
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-10-06 21:17:26 -0700

    installed mysqlclient; connected to db; hiding credentials

--- backend/odas_backend/odas_backend/settings.py
+++ backend/odas_backend/odas_backend/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'odas_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hidden',
+        'USER': 'hidden',
+        'PASSWORD': 'hidden',
+        'HOST': 'hidden',
+        'PORT': '3306',
     }
 }
 
