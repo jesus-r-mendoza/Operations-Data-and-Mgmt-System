c08c546180c5618735b162f6bd0de16c10ef0ee4
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-10-20 14:53:47 -0700

    created serializers for new models

--- backend/odas_backend/odas/serializers.py
+++ backend/odas_backend/odas/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from odas.models import Satellite
+from odas.models import Satellite, Component, Collectable, Measurement
 
 # Lead Serializer
 class SatelliteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Satellite
+        fields = '__all__'
+
+class ComponentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Component
+        fields = '__all__'
+
+class CollectableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collectable
+        fields = '__all__'
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
         fields = '__all__'
