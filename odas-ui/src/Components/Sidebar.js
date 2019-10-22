29b1f9a2a70fa680fad26f466927d63ebe9aec9d
Max <maxorozco213@gmail.com>
2019-10-21 23:02:23 -0700

    First iteration of checkboxes

--- odas-ui/src/Components/Sidebar.js
+++ odas-ui/src/Components/Sidebar.js
@@ -1,7 +1,10 @@
 import React from 'react';
 // Stylesheets
 import '../Layout/Reports.css'
-import {Button, Form, FormCheck} from "react-bootstrap";
+import {Button} from "react-bootstrap";
+import Checkbox from "./Checkbox";
+
+const LABELS = ["", "Temperature", "Voltage", "Velocity"];
 
 export default class QueryData extends React.Component {
 // TODO Implement the checkbox selections
@@ -10,17 +13,66 @@ export default class QueryData extends React.Component {
 
     constructor(props) {
         super(props);
+        // TODO map the attributes of the measurements table into this array
         this.state = {
             isLoading: true,
-            currentPage: this.props.page
+            currentPage: this.props.page,
+            checkboxes: LABELS.reduce(
+                (options, option) => ({
+                    ...options,
+                    [option]: false
+                }),
+            )
         };
-
         console.log(this.props.page)
     }
 
-    handleSubmit() {
-        console.log("chicken")
-    }
+    selectAllCheckboxes = isSelected => {
+        Object.keys(this.state.checkboxes).forEach(checkbox => {
+            this.setState(prevState => ({
+                checkboxes: {
+                    ...prevState.checkboxes,
+                    [checkbox]: isSelected
+                }
+            }));
+        });
+    };
+
+    selectAll = () => this.selectAllCheckboxes(true);
+
+    deselectAll = () => this.selectAllCheckboxes(false);
+
+    handleCheckboxChange = changeEvent => {
+        const { name } = changeEvent.target;
+
+        this.setState(prevState => ({
+            checkboxes: {
+                ...prevState.checkboxes,
+                [name]: !prevState.checkboxes[name]
+            }
+        }));
+    };
+
+    handleFormSubmit = formSubmitEvent => {
+        formSubmitEvent.preventDefault();
+
+        Object.keys(this.state.checkboxes)
+            .filter(checkbox => this.state.checkboxes[checkbox])
+            .forEach(checkbox => {
+                console.log(checkbox, "is selected.");
+            });
+    };
+
+    createCheckbox = option => (
+        <Checkbox
+            label={option}
+            isSelected={this.state.checkboxes[option]}
+            onCheckboxChange={this.handleCheckboxChange}
+            key={option}
+        />
+    );
+
+    createCheckboxes = () => LABELS.map(this.createCheckbox);
 
     goBack() {
         this.setState({
@@ -51,35 +103,30 @@ export default class QueryData extends React.Component {
     render() {
         return (
             <div className={"sidebar"}>
-                <div className={"sidebar-title"}>
-                    {/*{this.renderBackArrow(this.props.page)}*/}
-                    <span>{this.props.children}</span>
-                </div>
-                <div>
-                    <div className={"sidebar-info"}>
-                        <span>Select data to be reported</span>
+                <form onSubmit={this.handleFormSubmit}>
+                    <div className={"sidebar-title"}>
+                        {/*{this.renderBackArrow(this.props.page)}*/}
+                        <span>{this.props.children}</span>
                     </div>
-                    <div className={"checkbox-container"}>
-                        <Form>
-                            <FormCheck label={"Temperature"}/>
-                            <FormCheck label={"Voltage"}/>
-                            <FormCheck label={"Amps"}/>
-                            <FormCheck label={"Stuff"}/>
-                            <FormCheck label={"Stuff"}/>
-                            <FormCheck label={"Stuff"}/>
-                        </Form>
+                    <div>
+                        <div className={"sidebar-info"}>
+                            <span>Select data to be reported</span>
+                        </div>
+                        <div>
+
+                            {this.createCheckboxes()}
+                        </div>
+                        <div className={"gen-button-container"}>
+                            <Button
+                                type={"submit"}
+                                variant={"info"}
+                                className={"gen-button"}
+                            >
+                                Generate Report
+                            </Button>
+                        </div>
                     </div>
-                    <div className={"gen-button-container"}>
-                        <Button
-                            type={"submit"}
-                            variant={"info"}
-                            className={"gen-button"}
-                            onClick={() => this.handleSubmit()}
-                        >
-                            Generate Report
-                        </Button>
-                    </div>
-                </div>
+                </form>
             </div>
         );
     }
