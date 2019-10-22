0e5e77df2f2b085996aa3e5dde4054926a022157
Maximilian Orozco <maxorozco213@gmail.com>
2019-10-22 14:03:04 -0700

    Putting router in its own JS file

--- odas-ui/src/index.js
+++ odas-ui/src/index.js
@@ -1,33 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Route} from "react-router-dom";
-// Components
-import Header from "./Components/Header";
-import Main from "./Pages/Main";
-import QueryData from "./Pages/QueryData";
-import GenerateData from "./Pages/GenerateData";
-import UploadData from "./Pages/UploadData";
-// Stylesheets
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./Layout/Main.css";
+import App from "./Router"
 
-// TODO consider putting the router in its own file
-// TODO Consider turning into a nested route
-// TODO Reuse QueryData in GenerateData
-class App extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Header />
-                <div>
-                    <Route path={"/"} exact component={Main} />
-                    <Route path={"/query"} component={QueryData} />
-                    <Route path={"/generate"} component={GenerateData} />
-                    <Route path={"/upload"} component={UploadData} />
-                </div>
-            </Router>
-        );
-    }
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+);
