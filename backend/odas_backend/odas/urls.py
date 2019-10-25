727537bb7e47108bcfe3a7ce14fcacf8cbe8fac4
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-10-24 20:16:39 -0700

    mapped email sender to new url: <root>/email/

--- backend/odas_backend/odas/urls.py
+++ backend/odas_backend/odas/urls.py
@@ -13,7 +13,7 @@ router.register('api/units', UnitsViewSet, 'units')
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('email/', views.index, name='index'),
     path('success/', views.successView, name='success'),
     path('files/', views.file_view, name='file_list'),
     path('files/upload/', views.upload_view, name='upload_file'),
