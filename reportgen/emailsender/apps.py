831ad329641e07ae53b02c7d62d58d810f994eca
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-10-31 07:56:40 -0700

    removing unused folders/files

--- reportgen/emailsender/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmailsenderConfig(AppConfig):
-    name = 'emailsender'
--- reportgen/fileio/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FileioConfig(AppConfig):
-    name = 'fileio'
