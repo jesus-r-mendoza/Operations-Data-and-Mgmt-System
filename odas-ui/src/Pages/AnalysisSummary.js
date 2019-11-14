cd7401a27e280d18744fa2746b069d73b71754b8
Maximilian Orozco <maxorozco213@gmail.com>
2019-11-14 07:54:51 -0800

    Adding analysis page

---
+++ odas-ui/src/Pages/AnalysisSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import LoadSpinner from "../Components/LoadSpinner";
+import "../Layout/AnalysisSummary.css"
+
+export default class AnalysisSummary extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = ({
+            isLoading: true
+        });
+    }
+
+    render() {
+        if(this.state.isLoading) {
+            return (
+                <LoadSpinner />
+            )
+        }
+        return(
+            <div className={"document-page"}>
+                <h1>SUMMARY PAGE</h1>
+            </div>
+        );
+    }
+}
