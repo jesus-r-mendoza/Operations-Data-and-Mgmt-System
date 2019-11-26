b11de97e077fa4d1f085d2b19ce5e355075df4d5
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-11-26 15:50:34 -0800

    modified custom converter to use set instead of list, to ignore duplicate ids

--- backend/odas_backend/odas/converters.py
+++ backend/odas_backend/odas/converters.py
@@ -4,11 +4,12 @@ class ManyIdConverter:
     regex = '[\d+]+'
     
     def to_python(self, value):
-        res = [ int(id) for id in value.split('+') ]
+        res = { int(id) for id in value.split('+') }
         return res
 
     def to_url(self, value):
-        res = '+'.join(value)
+        strs = { f'{i}' for i in value }
+        res = '+'.join(strs)
         return res
 
 class DateTimeConverter:
