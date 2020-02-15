e12cab5f0ecc550495da2f9d45204509a8cac903
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-02-15 00:17:10 -0800

    added endpoint to listen for deployment

--- deployer/app.py
+++ deployer/app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, request
+import os
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello, from flask</h1>'
+@app.route('/deploy', methods=['POST'])
+def deploy():
+
+    with open('auth.cfg', 'r') as auth:
+        key = auth.read()
+
+    token = request.headers.get('Authorization')
+
+    if not token:
+        return 'You are not authorized to use this endpoint', 401
+
+    if token != key:
+        return 'Invalid Token', 403
+
+    if os.name == 'nt':
+        # windows
+        ignore_stdout = '> nul 2>&1'
+    else:
+        # posix or java
+        ignore_stdout = '> /dev/null'
+
+    rc = os.system(f'cd ~/Operations-Data-and-Mgmt-System {ignore_stdout}')
+    if rc != 0:
+        return 'Failed to change directory', 500
+
+    rc = os.system(f'git pull {ignore_stdout}')
+    if rc != 0:
+        return 'Failed update repo from github', 500
+
+    return 'Successfully deployed. Changes should occur shortly', 200
