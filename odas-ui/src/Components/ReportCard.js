f5c37b7431105a3a832f9e5b6e4128d8ba0d6da1
nlee806 <nlee806@gmail.com>
2020-02-15 10:33:52 -0800

    Initialized bottom tabular chart.

--- odas-ui/src/Components/ReportCard.js
+++ odas-ui/src/Components/ReportCard.js
@@ -59,6 +59,7 @@ export default class ReportCard extends React.Component {
 					revision={this.state.revision}
 					graphDiv = "graph"
 					/>
+					<BottomGraph/>
                 </div>
             </div>
         );
@@ -87,6 +88,22 @@ export default class ReportCard extends React.Component {
 				</div>
 				</div>
 			);
+		}
+	}
+	class BottomGraph extends React.Component{
+		render(){
+				return(
+					<div id = "bottomchart">
+					<p id="tablechart">Table Chart</p>
+					<button id="pause">Stop Graph</button>
+					<table style={{width:'100%'}} id = "traces">
+						<tr id = "fieldnames">
+						</tr>
+						<tr id = "data">
+						</tr>
+						</table>
+					</div>
+				);
 		}
 	}
 	
