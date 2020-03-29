e20fcdf1ded9a7aee7d09a22e337de8bed9d3e65
nlee806 <nlee806@gmail.com>
2020-03-29 15:04:06 -0700

    Reordered Valid Indices for easier access when inputting index.

--- odas-ui/src/Components/BottomGraph.js
+++ odas-ui/src/Components/BottomGraph.js
@@ -9,12 +9,12 @@ export default class BottomGraph extends React.Component{
 					<p id="tablechart">Table Chart</p>
 					<button id="pause" onClick={this.props.pause}>Stop Graph</button>
 					<div>
-						<label for="fname" id="chooseUnitLabel"><b> Input Test Index (must be valid, check Valid Indices and type here; if not known, put 0) :  </b></label>
-						<input type="text" id="chooseUnit" name="fname" defaultValue="0"></input>
-					</div>
-					<div>
 						<label for="validIndices" id="validIndicesLabel"><b> Valid Indices: </b></label>
 						<select id="validIndices"></select>
+					</div>
+					<div>
+						<label for="fname" id="chooseUnitLabel"><b> Input Test Index (must be valid, check Valid Indices and type here; if not known, put 0) :  </b></label>
+						<input type="text" id="chooseUnit" name="fname" defaultValue="0"></input>
 					</div>
 					<table style={{width:'100%'}} id = "traces">
 						<tbody id = "tracesbody">
