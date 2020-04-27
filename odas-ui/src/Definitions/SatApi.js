5bcff4e78d53aef28e7e91593825966e48743274
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2020-04-27 13:29:52 -0700

    changed Backend API Root URL to request from localhost; to default running on localmachine w/ default config file

--- odas-ui/src/Definitions/SatApi.js
+++ odas-ui/src/Definitions/SatApi.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-// const apiURL = "http://localhost:8080/";
-const apiURL = 'http://ecst-csproj2.calstatela.edu:6338/';
+// This endpoint will work when running the ODAS on local host,
+// using the default_public_config settings for the backend.
+const apiURL = "http://localhost:8080/";
+
+// const apiURL = 'http://ecst-csproj2.calstatela.edu:6338/';
+// const apiURL = 'http://localhost:8000/';
 
 // Can be used for generic API calls such as get requests
 export default axios.create({
